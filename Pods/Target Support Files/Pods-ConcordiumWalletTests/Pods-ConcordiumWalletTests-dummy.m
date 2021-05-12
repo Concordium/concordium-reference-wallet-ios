b771976e0f05df4cecd17c28a11d2d2d8c1574a5
Dennis Vexborg Kristensen <dkr@mjolner.dk>
2021-05-12 13:02:39 +0200

    Remove pods from source control

--- Pods/Target Support Files/Pods-ConcordiumWalletTests/Pods-ConcordiumWalletTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ConcordiumWalletTests : NSObject
-@end
-@implementation PodsDummy_Pods_ConcordiumWalletTests
-@end
--- Pods/Target Support Files/Pods-ProdMainNet/Pods-ProdMainNet-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ProdMainNet : NSObject
-@end
-@implementation PodsDummy_Pods_ProdMainNet
-@end
