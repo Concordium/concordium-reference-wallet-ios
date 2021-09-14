974a21a5b2dd49661f7a965b42bf861092288de5
Kristiyan Dobrev <kid@mjolner.dk>
2021-09-14 15:05:43 +0200

    Auto-generate CommitmentsRandomness model

---
+++ ConcordiumWallet/Model/AutoGenerated/CommitmentsRandomness.swift
@@ -0,0 +1,58 @@
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let commitmentsRandomness = try CommitmentsRandomness(json)
+
+import Foundation
+
+// MARK: - CommitmentsRandomness
+struct CommitmentsRandomness: Codable {
+    let attributesRand: [String: String]
+    let credCounterRand: String
+    let idCredSecRand: String
+    let maxAccountsRand: String
+    let prfRand: String
+}
+
+// MARK: CommitmentsRandomness convenience initializers and mutators
+
+extension CommitmentsRandomness {
+    init(data: Data) throws {
+        self = try newJSONDecoder().decode(CommitmentsRandomness.self, from: data)
+    }
+
+    init(_ json: String, using encoding: String.Encoding = .utf8) throws {
+        guard let data = json.data(using: encoding) else {
+            throw NSError(domain: "JSONDecoding", code: 0, userInfo: nil)
+        }
+        try self.init(data: data)
+    }
+
+    init(fromURL url: URL) throws {
+        try self.init(data: try Data(contentsOf: url))
+    }
+
+    func with(
+        attributesRand: [String: String]? = nil,
+        credCounterRand: String? = nil,
+        idCredSecRand: String? = nil,
+        maxAccountsRand: String? = nil,
+        prfRand: String? = nil
+    ) -> CommitmentsRandomness {
+        return CommitmentsRandomness(
+            attributesRand: attributesRand ?? self.attributesRand,
+            credCounterRand: credCounterRand ?? self.credCounterRand,
+            idCredSecRand: idCredSecRand ?? self.idCredSecRand,
+            maxAccountsRand: maxAccountsRand ?? self.maxAccountsRand,
+            prfRand: prfRand ?? self.prfRand
+        )
+    }
+
+    func jsonData() throws -> Data {
+        return try newJSONEncoder().encode(self)
+    }
+
+    func jsonString(encoding: String.Encoding = .utf8) throws -> String? {
+        return String(data: try self.jsonData(), encoding: encoding)
+    }
+}
