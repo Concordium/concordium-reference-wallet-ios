7472fbb64f8c62389489f24e679b4276c87b4f74
Kristiyan Dobrev <kid@mjolner.dk>
2021-09-27 09:16:16 +0200

    Rename let var

--- ioscommon/Commons/Data+Additions.swift
+++ ioscommon/Commons/Data+Additions.swift
@@ -22,14 +22,14 @@ extension Data {
         var data = Data(capacity: length)
         var index = hex.startIndex
         for _ in 0..<length {
-            let j = hex.index(index, offsetBy: 2)
-            let bytes = hex[index..<j]
+            let end = hex.index(index, offsetBy: 2)
+            let bytes = hex[index..<end]
             if var num = UInt8(bytes, radix: 16) {
                 data.append(&num, count: 1)
             } else {
                 return nil
             }
-            index = j
+            index = end
         }
         self = data
     }
