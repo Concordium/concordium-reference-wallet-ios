83737d82e7b2a468c9fb1a2138c3bf015bad6660
Kristiyan Dobrev <kid@mjolner.dk>
2021-11-08 14:57:28 +0100

    Syntax sugar expression

--- ioscommon/Commons/Publisher+Utils.swift
+++ ioscommon/Commons/Publisher+Utils.swift
@@ -35,7 +35,7 @@ extension Publisher {
     }
     
     func withPrevious() -> AnyPublisher<(previous: Output?, current: Output), Failure> {
-        scan(Optional<(Output?, Output)>.none) { ($0?.1, $1) }
+        scan((Output?, Output)?.none) { ($0?.1, $1) }
             .compactMap { $0 }
             .eraseToAnyPublisher()
     }
