5b429084e3668af9ea549db663acde824c84bd5e
Kristiyan Dobrev <kid@mjolner.dk>
2021-11-10 15:22:05 +0100

    AccountSubmissionStatus model wrapper

---
+++ ConcordiumWallet/Model/AccountSubmissionStatus.swift
@@ -0,0 +1,14 @@
+//
+//  AccountSubmissionStatus.swift
+//  ConcordiumWallet
+//
+//  Created by Kristiyan Dobrev on 10/11/2021.
+//  Copyright © 2021 concordium. All rights reserved.
+//
+
+import Foundation
+
+struct AccountSubmissionStatus {
+    let status: SubmissionStatusEnum
+    let account: AccountDataType
+}
