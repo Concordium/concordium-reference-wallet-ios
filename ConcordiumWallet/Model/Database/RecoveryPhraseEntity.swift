ade84d877ee0a26e81de0b2d21f4ac20ae4d48e8
Niels Chr. Friis Jakobsen <nfj@mjolner.dk>
2022-07-25 11:11:05 +0200

    remove unused file

--- ConcordiumWallet/Model/Database/RecoveryPhraseEntity.swift
+++
@@ -1,9 +0,0 @@
-//
-//  RecoveryPhraseEntity.swift
-//  ConcordiumWallet
-//
-//  Created by Niels Christian Friis Jakobsen on 30/06/2022.
-//  Copyright © 2022 concordium. All rights reserved.
-//
-
-import Foundation
