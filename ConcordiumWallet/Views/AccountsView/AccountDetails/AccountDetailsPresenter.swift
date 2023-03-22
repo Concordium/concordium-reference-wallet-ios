c4f44853a52e7ec7503a2ea11c48c7fef88daefb
etnavn <en@email.com>
2023-03-22 11:27:07 +0100

    Work

--- ConcordiumWallet/Views/AccountsView/AccountDetails/AccountDetailsPresenter.swift
+++ ConcordiumWallet/Views/AccountsView/AccountDetails/AccountDetailsPresenter.swift
@@ -314,10 +314,10 @@ extension AccountDetailsPresenter: AccountDetailsPresenterProtocol {
                     guard let self = self else { return }
 
                     // Any changes since last auto update?
-                    let equal = zip(transactionListAll, self.lastTransactionListAll)
+                    let equalUnmapped = zip(transactionListAll, self.lastTransactionListAll)
                         .enumerated()
                         .filter { $1.0.details.transactionHash == $1.1.details.transactionHash && $1.0.status == $1.1.status }
-                        .map { $1.0 }
+                    let equal = equalUnmapped.map { $1.0 }
 
                     if equal.count != transactionListAll.count {
                         self.lastTransactionListAll = transactionListAll
