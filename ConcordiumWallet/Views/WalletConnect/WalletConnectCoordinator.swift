c9f921a679061dc7036ae2c8bab379f1342e3aad
Milan Sawicki <milan.sawicki@me.com>
2023-06-20 17:16:48 +0200

    add payload object

--- ConcordiumWallet/Views/WalletConnect/WalletConnectCoordinator.swift
+++ ConcordiumWallet/Views/WalletConnect/WalletConnectCoordinator.swift
@@ -36,18 +36,36 @@ enum Schema: Decodable {
     }
 }
 
+
+// MARK: - Payload
+struct ContractUpdatePayload: Codable {
+    let amount: String
+    let address: Address
+    let receiveName: String
+    let maxContractExecutionEnergy: Int
+    let message: String
+}
+
+// MARK: - Address
+struct Address: Codable {
+    let index, subindex: Int
+}
+
 struct ContractUpdateParams: Decodable {
     let schema: Schema
     let type: TransferType
     let sender: String
-    let payload: [String: Any]
+    let payload: ContractUpdatePayload
     enum CodingKeys: String, CodingKey {
-        case schema, type
+        case schema, type, sender, payload
     }
 
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         type = try container.decode(TransferType.self, forKey: .type)
+        sender = try container.decode(String.self, forKey: .sender)
+        let payloadData = try Data(container.decode(String.self, forKey: .payload).utf8)
+        payload = try JSONDecoder().decode(ContractUpdatePayload.self, from: payloadData)
         // TODO: Check what the version should be if that's a string
         if let schema = try? container.decode(Schema.self, forKey: .schema) {
             self.schema = schema
