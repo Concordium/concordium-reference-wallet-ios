59785c8758e5ae1d6122944f10f972e1fde25a6d
Milan Sawicki <milan.sawicki@me.com>
2023-08-02 12:22:53 +0200

    initial commit

--- ConcordiumWallet/Views/AccountsView/AccountDetails/AccountDetailsViewController.swift
+++ ConcordiumWallet/Views/AccountsView/AccountDetails/AccountDetailsViewController.swift
@@ -6,14 +6,28 @@
 //  Copyright © 2020 concordium. All rights reserved.
 //
 
-import UIKit
 import Combine
 import SwiftUI
+import UIKit
+
+struct AccountTokensView: View {
+    var tabBarViewModel: MaterialTabBar.ViewModel = .init()
+
+    init() {
+        tabBarViewModel.tabs = ["Fungible", "Collectibles", "Manage"]
+    }
+
+    var body: some View {
+        VStack {
+            MaterialTabBar(viewModel: tabBarViewModel)
+        }
+    }
+}
 
 class AccountDetailsFactory {
     class func create(with presenter: AccountDetailsPresenter) -> AccountDetailsViewController {
         AccountDetailsViewController.instantiate(fromStoryboard: "Account") { coder in
-            return AccountDetailsViewController(coder: coder, presenter: presenter)
+            AccountDetailsViewController(coder: coder, presenter: presenter)
         }
     }
 }
@@ -28,66 +42,67 @@ class AccountDetailsViewController: BaseViewController, AccountDetailsViewProtoc
     private var shieldEnabled: Bool = false
     private var viewModel: AccountDetailsViewModel!
 
-    @IBOutlet weak var tabBar: UIView!
-    @IBOutlet weak var totalsStackView: UIStackView!
-    @IBOutlet weak var retryCreateButton: StandardButton!
-    @IBOutlet weak var removeLocalAccountButton: StandardButton!
-    
-    @IBOutlet weak var errorMessageLabel: UILabel!
-    @IBOutlet weak var statusImageView: UIImageView!
-    @IBOutlet weak var containerView: UIView!
-    
-    @IBOutlet weak var backgroundShield: UIImageView!
-    
-    @IBOutlet weak var readOnlyView: UIView!
-    @IBOutlet weak var balanceNameLabel: UILabel!
-    @IBOutlet weak var balanceLabel: UILabel!
-    @IBOutlet weak var atDisposalView: UIView!
-    @IBOutlet weak var stakedView: UIView!
-    
-    @IBOutlet weak var atDisposalLabel: UILabel!
-    @IBOutlet weak var stakedValueLabel: UILabel!
-    @IBOutlet weak var stakedLabel: UILabel!
-    
-    @IBOutlet weak var buttonsView: UIView!
-    @IBOutlet weak var generalButton: UIButton!
-    @IBOutlet weak var shieldedButton: UIButton!
-    @IBOutlet weak var spacerView: UIView!
-    @IBOutlet weak var topSpacingStackViewConstraint: NSLayoutConstraint!
-    @IBOutlet weak var buttonSliderContainer: RoundedCornerView!
-    
-    @IBOutlet weak var gtuDropView: UIView! {
+    @IBOutlet var tabBar: UIView!
+    @IBOutlet var totalsStackView: UIStackView!
+    @IBOutlet var retryCreateButton: StandardButton!
+    @IBOutlet var removeLocalAccountButton: StandardButton!
+
+    @IBOutlet var errorMessageLabel: UILabel!
+    @IBOutlet var statusImageView: UIImageView!
+    @IBOutlet var containerView: UIView!
+
+    @IBOutlet var backgroundShield: UIImageView!
+
+    @IBOutlet var readOnlyView: UIView!
+    @IBOutlet var balanceNameLabel: UILabel!
+    @IBOutlet var balanceLabel: UILabel!
+    @IBOutlet var atDisposalView: UIView!
+    @IBOutlet var stakedView: UIView!
+
+    @IBOutlet var atDisposalLabel: UILabel!
+    @IBOutlet var stakedValueLabel: UILabel!
+    @IBOutlet var stakedLabel: UILabel!
+
+    @IBOutlet var buttonsView: UIView!
+    @IBOutlet var generalButton: UIButton!
+    @IBOutlet var shieldedButton: UIButton!
+    @IBOutlet var spacerView: UIView!
+    @IBOutlet var topSpacingStackViewConstraint: NSLayoutConstraint!
+    @IBOutlet var buttonSliderContainer: RoundedCornerView!
+    private var accountTokensViewController = UIHostingController(rootView: AccountTokensView())
+    @IBOutlet var gtuDropView: UIView! {
         didSet {
             gtuDropView.isHidden = true
         }
     }
-    
+
     var identityDataVC: AccountDetailsIdentityDataViewController!
     var transactionsVC: AccountTransactionsDataViewController!
-    
+
     private var cancellables: [AnyCancellable] = []
-    
+
     init?(coder: NSCoder, presenter: AccountDetailsPresenterProtocol) {
         self.presenter = presenter
         super.init(coder: coder)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.gtuDropView.isHidden = true
-        
+        gtuDropView.isHidden = true
+
         setupTabBar()
         setupIdentityDataUI()
         setupTransactionsUI()
+
         title = presenter.getTitle()
         presenter.view = self
         presenter.viewDidLoad()
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         presenter.viewWillAppear()
@@ -102,35 +117,35 @@ class AccountDetailsViewController: BaseViewController, AccountDetailsViewProtoc
                                                name: UIApplication.willResignActiveNotification,
                                                object: nil)
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         stopRefreshTimer()
-        if self.isMovingFromParent {
+        if isMovingFromParent {
             presenter.viewWillDisappear()
         }
         NotificationCenter.default.removeObserver(self, name: UIApplication.didBecomeActiveNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIApplication.willResignActiveNotification, object: nil)
     }
-    
+
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        if self.isMovingFromParent {
+        if isMovingFromParent {
             cancellables = []
             identityDataVC = nil
             transactionsVC = nil
         }
     }
-    
+
     @objc func appDidBecomeActive() {
         presenter.updateTransfersOnChanges()
         startRefreshTimer()
     }
-    
+
     @objc func appWillResignActive() {
         stopRefreshTimer()
     }
-    
+
     func startRefreshTimer() {
         updateTimer = Timer.scheduledTimer(timeInterval: 5.0,
                                            target: self,
@@ -138,30 +153,40 @@ class AccountDetailsViewController: BaseViewController, AccountDetailsViewProtoc
                                            userInfo: nil,
                                            repeats: true)
     }
-    
+
     func stopRefreshTimer() {
         if updateTimer != nil {
             updateTimer?.invalidate()
             updateTimer = nil
         }
     }
-    
+
     @objc func refreshOnTimerCallback() {
         presenter.updateTransfersOnChanges()
     }
-    
+
     private func setupButtonSlider(isShielded: Bool) {
         let areActionsEnabled = viewModel.accountState == .finalized && !viewModel.isReadOnly
-        
+
         let buttonSlider = ButtonSlider(
             isShielded: isShielded,
-            actionTokens: {
-                // TODO
+            didTapTokensButton: { [weak self] in
+                self?.viewModel.selectedSection = .tokens
+                self?.transactionsVC.view.isHidden = true
+                self?.tabBar.isHidden = true
+                self?.accountTokensViewController.view.isHidden = false
             },
             actionSend: {
                 if self.sendEnabled {
                     self.presenter.userTappedSend()
                 }
+            },
+            didTapTransactionList: { [weak self] in
+                self?.viewModel.selectedSection = .transfers
+                self?.transactionsVC.view.isHidden = false
+//                self?.tabBar.isHidden = false
+                self?.accountTokensViewController.view.isHidden = true
+
             },
             actionReceive: {
                 if self.receiveEnabled {
@@ -179,14 +204,10 @@ class AccountDetailsViewController: BaseViewController, AccountDetailsViewProtoc
             actionSettings: {
                 self.presenter.burgerButtonTapped()
             },
+            selectedSection: viewModel.selectedSection,
             isDisabled: !areActionsEnabled
         )
-        let childView = UIHostingController(rootView: buttonSlider)
-        addChild(childView)
-        childView.view.frame = buttonSliderContainer.bounds
-        buttonSliderContainer.subviews.forEach { $0.removeFromSuperview() }
-        buttonSliderContainer.addSubview(childView.view)
-        childView.didMove(toParent: self)
+        show(buttonSlider, in: buttonSliderContainer)
     }
 
     private func setupButtonsShielded() {
@@ -206,20 +227,15 @@ class AccountDetailsViewController: BaseViewController, AccountDetailsViewProtoc
                     self.presenter.userTappedAddress()
                 }
             })
-        let childView = UIHostingController(rootView: buttonsShielded)
-        addChild(childView)
-        childView.view.frame = buttonSliderContainer.bounds
-        buttonSliderContainer.subviews.forEach { $0.removeFromSuperview() }
-        buttonSliderContainer.addSubview(childView.view)
-        childView.didMove(toParent: self)
+        show(buttonsShielded, in: buttonSliderContainer)
     }
 
     // swiftlint:disable function_body_length
     func bind(to viewModel: AccountDetailsViewModel) {
         self.viewModel = viewModel
-        
-        self.showTransferData(accountState: viewModel.accountState, isReadOnly: viewModel.isReadOnly, hasTransfers: viewModel.hasTransfers)
-        
+
+        showTransferData(accountState: viewModel.accountState, isReadOnly: viewModel.isReadOnly, hasTransfers: viewModel.hasTransfers)
+
         tabViewModel.$selectedIndex
             .sink { [weak self] index in
                 if index == 0 {
@@ -235,35 +251,35 @@ class AccountDetailsViewController: BaseViewController, AccountDetailsViewProtoc
                 }
             }
             .store(in: &cancellables)
-        
+
         viewModel.$selectedBalance
             .sink { [weak self] _ in
                 self?.tabViewModel.selectedIndex = 0
             }
             .store(in: &cancellables)
-        
+
         Publishers.CombineLatest(viewModel.$hasTransfers, viewModel.$accountState)
-            .sink { [weak self](hasTransfers: Bool, accountState: SubmissionStatusEnum) in
+            .sink { [weak self] (hasTransfers: Bool, accountState: SubmissionStatusEnum) in
                 self?.showTransferData(accountState: accountState, isReadOnly: viewModel.isReadOnly, hasTransfers: hasTransfers)
             }.store(in: &cancellables)
-        
+
         viewModel.$balance
             .compactMap { $0 }
             .assign(to: \.text, on: balanceLabel)
             .store(in: &cancellables)
-        
-        viewModel.$isShielded.sink { [weak self](isShielded) in
+
+        viewModel.$isShielded.sink { [weak self] isShielded in
             guard let self = self else { return }
 
             self.isShielded = isShielded
             self.title = self.presenter.getTitle()
             self.atDisposalView.setHiddenIfChanged(isShielded)
-            
+
             self.generalButton.backgroundColor = isShielded ? UIColor.primary : UIColor.primarySelected
             self.shieldedButton.backgroundColor = isShielded ? UIColor.primarySelected : UIColor.primary
-            
+
             if isShielded {
-                self.balanceNameLabel.text =  String(format: ("accounts.overview.shieldedtotal".localized), viewModel.name ?? "")
+                self.balanceNameLabel.text = String(format: "accounts.overview.shieldedtotal".localized, viewModel.name ?? "")
                 self.stakedView.setHiddenIfChanged(true)
             } else {
                 self.balanceNameLabel.text = "accounts.overview.generaltotal".localized
@@ -287,7 +303,7 @@ class AccountDetailsViewController: BaseViewController, AccountDetailsViewProtoc
                 self.view.layoutIfNeeded()
             }
         }.store(in: &cancellables)
-        
+
         viewModel.$isShieldedEnabled.sink { [weak self] enabled in
             if enabled {
                 self?.buttonsView.setHiddenIfChanged(false)
@@ -297,23 +313,23 @@ class AccountDetailsViewController: BaseViewController, AccountDetailsViewProtoc
                 self?.spacerView.setHiddenIfChanged(false)
             }
         }.store(in: &cancellables)
-        
+
         viewModel.$atDisposal
             .compactMap { $0 }
             .assign(to: \.text, on: atDisposalLabel)
             .store(in: &cancellables)
-        
+
         viewModel.$stakedValue
             .compactMap { $0 }
             .assign(to: \.text, on: stakedValueLabel)
             .store(in: &cancellables)
-        
+
         viewModel.$stakedLabel
-            .sink { [weak self](text) in
+            .sink { [weak self] text in
                 self?.stakedLabel.text = text
             }
             .store(in: &cancellables)
-        
+
         viewModel.$isReadOnly
             .map { !$0 }
             .sink(receiveValue: { [weak self] isReadOnly in
@@ -321,29 +337,29 @@ class AccountDetailsViewController: BaseViewController, AccountDetailsViewProtoc
             })
             .store(in: &cancellables)
     }
-    
+
     @IBAction func retryAccountCreationTapped(_ sender: Any) {
         presenter.userTappedRetryAccountCreation()
     }
-    
+
     @IBAction func removeFailedLocalAccountTapped(_ sender: Any) {
         presenter.userTappedRemoveFailedAccount()
     }
-    
+
     @IBAction func gtuDropTapped(_ sender: UIButton) {
         sender.isEnabled = false
         presenter.gtuDropTapped()
     }
-    
+
     @IBAction func pressedUnlock(_ sender: UIBarButtonItem) {
         sender.isEnabled = false
         presenter.pressedUnlock()
     }
-    
+
     @IBAction func pressedGeneral(_ sender: UIButton) {
         presenter.userSelectedGeneral()
     }
-    
+
     @IBAction func pressedShielded(_ sender: UIButton) {
         presenter.userSelectedShieled()
     }
@@ -352,22 +368,22 @@ class AccountDetailsViewController: BaseViewController, AccountDetailsViewProtoc
 extension AccountDetailsViewController {
     func setupTabBar() {
         tabViewModel.tabs = [
-            "accountDetails.transfers".localized
+            "accountDetails.transfers".localized,
         ]
-        
+
         show(MaterialTabBar(viewModel: tabViewModel), in: tabBar)
     }
-    
+
     fileprivate func setupIdentityDataUI() {
         identityDataVC = AccountDetailsIdentityDataFactory.create(with: presenter.getIdentityDataPresenter())
         add(child: identityDataVC, inside: containerView)
     }
-    
+
     fileprivate func setupTransactionsUI() {
         transactionsVC = AccountTransactionsDataFactory.create(with: presenter.getTransactionsDataPresenter())
         add(child: transactionsVC, inside: containerView)
     }
-    
+
     fileprivate func showIdentityData() {
         transactionsVC.view.isHidden = true
         if identityDataVC.hasIdentities() {
@@ -377,17 +393,17 @@ extension AccountDetailsViewController {
             errorMessageLabel.text = "accountDetails.noIdentities".localized
         }
     }
-    
+
     private func showTransferData(accountState: SubmissionStatusEnum, isReadOnly: Bool, hasTransfers: Bool) {
         identityDataVC.view.isHidden = true
-        self.setupUIBasedOn(accountState, isReadOnly: isReadOnly)
+        setupUIBasedOn(accountState, isReadOnly: isReadOnly)
         if accountState == .finalized {
-            self.updateTransfersUI(hasTransfers: hasTransfers)
+            updateTransfersUI(hasTransfers: hasTransfers)
         } else {
             transactionsVC.view.isHidden = true
         }
     }
-    
+
     fileprivate func updateTransfersUI(hasTransfers: Bool) {
         // If no transfers show message
         if hasTransfers {
@@ -397,23 +413,23 @@ extension AccountDetailsViewController {
             transactionsVC.view.isHidden = true
             errorMessageLabel.text = "accountDetails.noTransfers".localized
         }
-        
-#if ENABLE_GTU_DROP
-        if hasTransfers {
-            self.gtuDropView.isHidden = true
-            errorMessageLabel.superview?.isHidden = false
-        } else {
-            if presenter.showGTUDrop() {
-                self.gtuDropView.isHidden = false
-                errorMessageLabel.superview?.isHidden = true
-            } else {
-                self.gtuDropView.isHidden = true
+
+        #if ENABLE_GTU_DROP
+            if hasTransfers {
+                gtuDropView.isHidden = true
                 errorMessageLabel.superview?.isHidden = false
+            } else {
+                if presenter.showGTUDrop() {
+                    gtuDropView.isHidden = false
+                    errorMessageLabel.superview?.isHidden = true
+                } else {
+                    gtuDropView.isHidden = true
+                    errorMessageLabel.superview?.isHidden = false
+                }
             }
-        }
-#endif
+        #endif
     }
-    
+
     fileprivate func setupUIBasedOn(_ state: SubmissionStatusEnum, isReadOnly: Bool) {
         var showMessage = false
         var message = ""
@@ -421,9 +437,9 @@ extension AccountDetailsViewController {
         var statusIconImageName = ""
         var canSend = false
         switch state {
-            // received and committed should be handled the same way
-            // Difference is that the committed state will have a block has because it is on a block (but no yet finalized).
-            // received state is not yet in a block. Both can be interpreted as "pending" state
+        // received and committed should be handled the same way
+        // Difference is that the committed state will have a block has because it is on a block (but no yet finalized).
+        // received state is not yet in a block. Both can be interpreted as "pending" state
         case .committed, .received:
             showMessage = true
             message = "accountDetails.committedMessage".localized
@@ -438,12 +454,12 @@ extension AccountDetailsViewController {
                 canSend = true
             }
         }
-        self.retryCreateButton.isHidden = !showErrorButtons
-        self.removeLocalAccountButton.isHidden = !showErrorButtons
-        self.errorMessageLabel.text = message
-        self.statusImageView.isHidden = !showMessage
+        retryCreateButton.isHidden = !showErrorButtons
+        removeLocalAccountButton.isHidden = !showErrorButtons
+        errorMessageLabel.text = message
+        statusImageView.isHidden = !showMessage
         if !statusIconImageName.isEmpty {
-            self.statusImageView.image = UIImage(named: statusIconImageName)
+            statusImageView.image = UIImage(named: statusIconImageName)
         }
         // Disable send and address if not finalized
         if canSend {
