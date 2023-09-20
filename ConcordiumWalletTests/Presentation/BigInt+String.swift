057088ad135e5b224b733d00043d9fc016e94526
Milan Sawicki <milan.sawicki@me.com>
2023-09-20 14:23:39 +0200

    add missing test

--- ConcordiumWalletTests/Presentation/BigInt+String.swift
+++ ConcordiumWalletTests/Presentation/BigInt+String.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2023 concordium. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import Mock
+import BigInt
+class IntegerFormattingTests: XCTestCase {
+
+    func testFormattingWithTrailingZeros() {
+        XCTAssertEqual(BigInt(12345000000).formatIntegerWithFractionDigits(fractionDigits: 6), "12345")
+        XCTAssertEqual(BigInt(12345000000).formatIntegerWithFractionDigits(fractionDigits: 7), "1234.5")
+    }
+    
+    func testEdgeCases() {
+        XCTAssertEqual(BigInt(0).formatIntegerWithFractionDigits(fractionDigits: 2), "0")
+        XCTAssertEqual(BigInt(0).formatIntegerWithFractionDigits(fractionDigits: 0), "0")
+    }
+}
