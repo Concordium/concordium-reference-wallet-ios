12909602051c1203f7605f77c44ec2a251fe79bd
TD <blueturtledk@icloud.com>
2023-11-13 11:35:11 +0100

    fix pr comments

--- ConcordiumWallet/Views/Stake/Baking/ComissionSettings/BakerCommissionSettingsView.swift
+++ ConcordiumWallet/Views/Stake/Baking/ComissionSettings/BakerCommissionSettingsView.swift
@@ -10,7 +10,7 @@ import Combine
 import SwiftUI
 
 struct BakerCommissionSettingsView: View {
-    static var sliderStep = 1e-5
+    static var sliderStep = 1e-3
     @StateObject var viewModel: BakerCommissionSettingsViewModel
 
     var body: some View {
@@ -79,9 +79,9 @@ struct BakerCommissionSliderView: View {
             self.commission
         }, set: {
             // Rounding for edge cases of sliders to prevent odd values caused by floating point errors.
-            if $0 + BakerCommissionSettingsView.sliderStep > range.max {
+            if $0 + BakerCommissionSettingsView.sliderStep >= range.max {
                 commission = range.max
-            } else if $0 - BakerCommissionSettingsView.sliderStep < range.min {
+            } else if $0 - BakerCommissionSettingsView.sliderStep <= range.min {
                 commission = range.min
             } else {
                 self.commission = $0
@@ -117,105 +117,3 @@ struct BakerCommissionSliderView: View {
         .padding(.bottom, 16)
     }
 }
-
-#if DEBUG
-    fileprivate class StakeServiceMock: StakeServiceProtocol {
-        func getBakerPool(bakerId: Int) -> AnyPublisher<BakerPoolResponse, Error> {
-            .empty()
-        }
-
-        func getPassiveDelegation() -> AnyPublisher<PassiveDelegation, Error> {
-            .empty()
-        }
-
-        func getChainParameters() -> AnyPublisher<ChainParametersResponse, Error> {
-            .just(
-                try! ChainParametersResponse("""
-                    {
-                        "accountCreationLimit": 10,
-                        "bakingCommissionRange": {
-                            "max": 0.2,
-                            "min": 0.2
-                        },
-                        "blockEnergyLimit": 5000000,
-                        "capitalBound": 0.1,
-                        "delegatorCooldown": 900,
-                        "euroPerEnergy": {
-                            "denominator": 1100000,
-                            "numerator": 1
-                        },
-                        "finalizationCommissionRange": {
-                            "max": 0.95,
-                            "min": 5.0e-2
-                        },
-                        "finalizerRelativeStakeThreshold": 0.11,
-                        "foundationAccountIndex": 5,
-                        "leverageBound": {
-                            "denominator": 1,
-                            "numerator": 3
-                        },
-                        "maximumFinalizers": 6,
-                        "microGTUPerEuro": {
-                            "denominator": 44819431759,
-                            "numerator": 7776407313978818560
-                        },
-                        "minBlockTime": 2800,
-                        "minimumEquityCapital": "100000000",
-                        "minimumFinalizers": 3,
-                        "mintPerPayday": 1.582e-5,
-                        "passiveBakingCommission": 0.12,
-                        "passiveFinalizationCommission": 1.0,
-                        "passiveTransactionCommission": 0.12,
-                        "poolOwnerCooldown": 1800,
-                        "rewardParameters": {
-                            "gASRewards": {
-                                "accountCreation": 2.0e-2,
-                                "baker": 0.25,
-                                "chainUpdate": 5.0e-3
-                            },
-                            "mintDistribution": {
-                                "bakingReward": 0.6,
-                                "finalizationReward": 0.3
-                            },
-                            "transactionFeeDistribution": {
-                                "baker": 0.45,
-                                "gasAccount": 0.45
-                            }
-                        },
-                        "rewardPeriodLength": 4,
-                        "timeoutBase": 10000,
-                        "timeoutDecrease": {
-                            "denominator": 2,
-                            "numerator": 1
-                        },
-                        "timeoutIncrease": {
-                            "denominator": 2,
-                            "numerator": 3
-                        },
-                        "transactionCommissionRange": {
-                            "max": 0.8,
-                            "min": 0.2
-                        }
-                    }
-                    """
-                )
-            )
-        }
-
-        func generateBakerKeys() -> Result<GeneratedBakerKeys, Error> {
-            fatalError()
-        }
-    }
-
-struct BakerCommissionSettingsViewPreview: PreviewProvider {
-    static var previews: some View {
-        BakerCommissionSettingsView(
-            viewModel: .init(
-                service: StakeServiceMock(),
-                handler: StakeDataHandler(transferType: .configureBaker),
-                didTapContinue: { }
-            )
-        )
-    }
-}
-#endif
